5f696f8bb516db75032765304e207db3b8f4f20c
Tanzzzie <36549831+Tanzzzie@users.noreply.github.com>
2018-02-20 09:55:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,8 +63,8 @@ client.on("message", async message => {
     const deleteCount = parseInt(args[0], 10);
     
     
-    if(!deleteCount || deleteCount < 2 || deleteCount > 1000)
-      return message.reply("Please provide a number between 2 and 1000 for the number of messages to delete");
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
     
     
     const fetched = await message.channel.fetchMessages({count: deleteCount});
